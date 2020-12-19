0a4ca7d73ff3e464035e129fef710081ff633758
jxswin <ptjeswin99@gmail.com>
2020-12-19 20:40:53 +0530

    changed logo

--- resources/js/frontend/src/components/SidebarPlugin/SideBar.vue
+++ resources/js/frontend/src/components/SidebarPlugin/SideBar.vue
@@ -12,7 +12,7 @@
 
             <slot name="mobile-right">
                 <ul class="nav align-items-center d-md-none">
-                    <base-dropdown class="nav-item" position="right">
+                    <!-- <base-dropdown class="nav-item" position="right">
                         <a slot="title" class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown"
                            aria-haspopup="true" aria-expanded="false">
                             <i class="ni ni-bell-55"></i>
@@ -22,7 +22,7 @@
                         <a class="dropdown-item" href="#">Another action</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
-                    </base-dropdown>
+                    </base-dropdown> -->
                     <base-dropdown class="nav-item" position="right">
                         <a slot="title" class="nav-link" href="#" role="button">
                             <div class="media align-items-center">
--- resources/js/frontend/src/views/UserProfile.vue
+++ resources/js/frontend/src/views/UserProfile.vue
@@ -1,18 +1,16 @@
 <template>
     <div>
         <base-header class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-                     style="min-height: 600px; background-image: url(img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
-            <!-- Mask -->
-            <span class="mask bg-gradient-success opacity-8"></span>
-            <!-- Header container -->
+                     style="min-height: 100px; background-size: cover; background-position: center top;">
+            <span class="mask bg-gradient-dark opacity-10"></span>
             <div class="container-fluid d-flex align-items-center">
-                <div class="row">
+                <!-- <div class="row">
                     <div class="col-lg-7 col-md-10">
                         <h1 class="display-2 text-white">Hello Jesse</h1>
                         <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
                         <a href="#!" class="btn btn-info">Edit profile</a>
                     </div>
-                </div>
+                </div> -->
             </div>
         </base-header>
 
@@ -31,13 +29,13 @@
                             </div>
                         </div>
                         <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                            <div class="d-flex justify-content-between">
+                            <!-- <div class="d-flex justify-content-between">
                                 <base-button size="sm" type="info" class="mr-4">Connect</base-button>
                                 <base-button size="sm" type="default" class="float-right">Message</base-button>
-                            </div>
+                            </div> -->
                         </div>
-                        <div class="card-body pt-0 pt-md-4">
-                            <div class="row">
+                        <div class="card-body pt-md-7">
+                            <!-- <div class="row">
                                 <div class="col">
                                     <div class="card-profile-stats d-flex justify-content-center mt-md-5">
                                         <div>
@@ -54,8 +52,8 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="text-center">
+                            </div> -->
+                            <div class="text-center ">
                                 <h3>
                                     Jessica Jones<span class="font-weight-light">, 27</span>
                                 </h3>
@@ -63,124 +61,56 @@
                                     <i class="ni location_pin mr-2"></i>Bucharest, Romania
                                 </div>
                                 <div class="h5 mt-4">
-                                    <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+                                    <i class="ni business_briefcase-24 mr-2"></i>Created by
                                 </div>
                                 <div>
-                                    <i class="ni education_hat mr-2"></i>University of Computer Science
+                                    <i class="ni education_hat mr-2"></i>Gmart
                                 </div>
-                                <hr class="my-4" />
+                                <div>
+                                    <i class="ni education_hat mr-2"></i>at Mumbai
+                                </div>
+                                <!-- <hr class="my-4" />
                                 <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
-                                <a href="#">Show more</a>
+                                <a href="#">Show more</a> -->
                             </div>
                         </div>
                     </div>
+                     
                 </div>
-
+                                               
                 <div class="col-xl-8 order-xl-1">
                     <card shadow type="secondary">
                         <div slot="header" class="bg-white border-0">
                             <div class="row align-items-center">
                                 <div class="col-8">
-                                    <h3 class="mb-0">My account</h3>
+                                    <h3 class="mb-0">Proceed Payment</h3>
                                 </div>
-                                <div class="col-4 text-right">
+                                <!-- <div class="col-4 text-right">
                                     <a href="#!" class="btn btn-sm btn-primary">Settings</a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                         <template>
                             <form @submit.prevent>
-                                <h6 class="heading-small text-muted mb-4">User information</h6>
+                               
                                 <div class="pl-lg-4">
                                     <div class="row">
                                         <div class="col-lg-6">
                                             <base-input alternative=""
-                                                        label="Username"
-                                                        placeholder="Username"
+                                                        label="Amount"
+                                                        placeholder="Amount"
                                                         input-classes="form-control-alternative"
                                                         v-model="model.username"
                                             />
                                         </div>
-                                        <div class="col-lg-6">
-                                            <base-input alternative=""
-                                                        label="Email address"
-                                                        placeholder="jesse@example.com"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.email"
-                                            />
+                                        <div class=" col-lg-6 text-center">
+                                            <a href="#!" class="btn btn-md btn-primary px-5">Pay</a>
                                         </div>
+                                        
                                     </div>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <base-input alternative=""
-                                                        label="First name"
-                                                        placeholder="First name"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.firstName"
-                                            />
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <base-input alternative=""
-                                                        label="Last name"
-                                                        placeholder="Last name"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.lastName"
-                                            />
-                                        </div>
-                                    </div>
+                                    
                                 </div>
-                                <hr class="my-4" />
-                                <!-- Address -->
-                                <h6 class="heading-small text-muted mb-4">Contact information</h6>
-                                <div class="pl-lg-4">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <base-input alternative=""
-                                                        label="Address"
-                                                        placeholder="Home Address"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.address"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <base-input alternative=""
-                                                        label="City"
-                                                        placeholder="City"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.city"
-                                            />
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <base-input alternative=""
-                                                        label="Country"
-                                                        placeholder="Country"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.country"
-                                            />
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <base-input alternative=""
-                                                        label="Postal code"
-                                                        placeholder="Postal code"
-                                                        input-classes="form-control-alternative"
-                                                        v-model="model.zipCode"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr class="my-4" />
-                                <!-- Description -->
-                                <h6 class="heading-small text-muted mb-4">About me</h6>
-                                <div class="pl-lg-4">
-                                    <div class="form-group">
-                                        <base-input alternative=""
-                                                    label="About Me">
-                                            <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
-                                        </base-input>
-                                    </div>
-                                </div>
+                               
                             </form>
                         </template>
                     </card>
