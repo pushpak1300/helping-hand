f821fc825a37e4297a3b3bbb25c5c729dcfd8bb0
pushpak1300 <pushpak1300@gmail.com>
2020-12-19 12:50:29 +0530

    Init

--- config/app.php
+++ config/app.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
 
     /*
@@ -226,7 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
-
 ];
